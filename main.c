2e2286462d6e6ab6c6e000c1697d43b807a494fc
Nathan Santos <nathan_lucindosantos@hotmail.com>
2024-06-18 22:08:43 -0300

    subindo possíveis construtores

--- main.c
+++ main.c
@@ -4,30 +4,53 @@
 
 /* ===== STRUCTS EMBAIXO ===== */
 
-
-
-
-typedef struct {
+typedef struct
+{
     int movie_id;
-    struct node* next;
+    struct node *next;
 } node;
 
-typedef struct {
-    int id;
-    char* name;
-    struct node* movies;
-} actor;
-
-typedef struct {
-    int id;
-    char* title;
-    struct node* neighbors;
-} movie;
-
-struct node* create_node(int id) {
-    node* new_node = malloc(sizeof(node));
+struct node *create_node(int id)
+{
+    node *new_node = (node*) malloc (sizeof(node));
     new_node->movie_id = id;
     new_node->next = NULL;
+}
+
+
+
+typedef struct
+{
+    int id;
+    char *name;
+    struct node *movies;
+} actor;
+
+actor construtor_actor(int id, char *name, char *movies)
+{
+    actor new_actor;
+    new_actor.id = id;
+    new_actor.name = name;
+    new_actor.movies = movies;
+    return new_actor;
+}
+
+
+
+typedef struct
+{
+    int id;
+    char *title;
+    node *neighbors;
+} movie;
+
+movie construtor_movie(int id, char *title, node *next)
+{
+    movie new_movie;
+    new_movie.id = id;
+    new_movie.title = title;
+    new_movie.neighbors = next;
+    return new_movie;
 }
 
 
@@ -35,8 +58,12 @@ struct node* create_node(int id) {
 
 /* ===== STRUCTS EM CIMA ===== */
 
+
+
+
+
 int main()
 {
-    
+
     return 0;
 }
