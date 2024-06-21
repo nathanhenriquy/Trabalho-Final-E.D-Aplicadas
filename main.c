3e89a7b00099aff51e2361e398fafc6526e0fb65
Nathan Santos <nathan_lucindosantos@hotmail.com>
2024-06-21 15:41:57 -0300

    atualização das estruturas

--- main.c
+++ main.c
@@ -2,95 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct node {
-    int index_film;
+#define CAPACIDADE_INICIAL 10
+
+// ===== ESTRUTURA DE STRUCS E INICIALIZAÇÕES ABAIXO =====
+
+typedef struct Node {
+    int id;
+    struct Node *next;
+} Node;
+
+typedef struct {
     int id;
     char *name;
-    struct node *next;
-} node;
+    Node *movies;
+} Actor;
 
-node* construtor_node(int index, int id, char *name, node *next) {
-    node *new_node = (node*)malloc(sizeof(node));
-    new_node->index_film = index;
-    new_node->name = name;
-    new_node->id = id;
-    new_node->next = next;
-    return new_node;
-}
-
-
-//=== NODE ACIMA
-
-
-typedef struct{ 
+typedef struct {
     int id;
     char *title;
-    node *neighbors;
-} movie;
-
-movie construtor_movie(int id, char *title, node *next) {
-    movie new_movie;
-    new_movie.id = id;
-    new_movie.title = title;
-    new_movie.neighbors = next;
-    return new_movie;
-}
-
-
-//=== FILMES ACIMA
-
+    Node *neighbors;
+} Movie;
 
 typedef struct {
-    int id;
-    char *name;
-    node *movies;
-} actor;
-
-actor construtor_actor(int id, char *name, char *movies) {
-    actor new_actor;
-    new_actor.id = id;
-    new_actor.name = name;
-    new_actor.movies = movies;
-    return new_actor;
-}
-
-
-//=== ATORES ACIMA
-
-
-typedef struct {
-    int size;
-    int capacity;   
-    actor *actors;
-} array__actors;
-
-array__actors* construtor_array_actors() {
-    array__actors *new_array = (array__actors*)malloc(sizeof(array__actors));
-    new_array->capacity = 0;
-    new_array->size = 0;
-    new_array->actors = NULL;
-    return new_array;
-}
-
-
-//===ARRAY DE ATORES ACIMA
-
-
-
-typedef struct {
+    Actor *data;
     int size;
     int capacity;
-    movie *movies;
-} array__movies;
+} DynamicArray;
 
-array__movies*construtor_array_movie() {
-    array__movies *new_array = malloc(sizeof(array__movies));
-    new_array->size = 0;
-    new_array->capacity = 0;
-    new_array->movies = NULL;
-    return new_array;
+
+typedef struct {
+    Movie *data;
+    int size;
+    int capacity;
+} AdjList;
+
+void initArray(DynamicArray *a) {
+    a->data = malloc(CAPACIDADE_INICIAL * sizeof(Actor));
+    a->size = 0;
+    a->capacity = CAPACIDADE_INICIAL;
+}
+
+void insertArray(DynamicArray *a, Actor actor) {
+    if (a->size == a->capacity) {
+        a->capacity *= 2;
+        a->data = realloc(a->data, a->capacity * sizeof(Actor));
+    }
+    a->data[a->size++] = actor;
+}
+
+void initAdjList(AdjList *al) {
+    al->data = malloc(CAPACIDADE_INICIAL * sizeof(Movie));
+    al->size = 0;
+    al->capacity = CAPACIDADE_INICIAL;
+}
+
+void insertAdjList(AdjList *al, Movie movie) {
+    if (al->size == al->capacity) {
+        al->capacity *= 2;
+        al->data = realloc(al->data, al->capacity * sizeof(Movie));
+    }
+    al->data[al->size++] = movie;
 }
 
 
-//===ARRAY DE FILMES ACIMA
+
+// ===== ESTRUTURA DE STRUCS E INICIALIZAÇÕES A CIMA =====
+
 
